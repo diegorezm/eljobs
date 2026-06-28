f4d79c2cc895cfe528227026b9cb2504d3e372a0
diegorezm <dmourarezende@gmail.com>
2026-06-28 18:39:57 -0300

    initializing the modules

--- lib/eljobs.ex
+++ lib/eljobs.ex
@@ -1,4 +1,13 @@
 defmodule Eljobs do
-  def start do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      Dispatcher,
+      WorkerSupervisor,
+      {Bandit, plug: Router, port: 4000}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
