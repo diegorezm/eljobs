58a4ec1ba7cb920d0c6943f471cd4c682dd869cc
diegorezm <dmourarezende@gmail.com>
2026-06-28 11:17:20 -0300

    added the worker supervisor

---
+++ lib/worker_supervisor.ex
@@ -0,0 +1,19 @@
+defmodule WorkerSupervisor do
+  use Supervisor
+
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_init_arg) do
+    children = [
+      {Worker, UUID.uuid4()},
+      {Worker, UUID.uuid4()},
+      {Worker, UUID.uuid4()},
+      {Worker, UUID.uuid4()}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
